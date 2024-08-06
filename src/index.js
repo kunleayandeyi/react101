3ed1809e1222c6d0ec0d3faa87ce2ac6520e8c3b
ksquare <kunleayandeyi@gmail.com>
2024-08-06 22:02:06 +0100

    component and jsx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 const App = function () {
-  return <h1>Hello react world welcome on board</h1>;
+  return (
+    <div>
+      <h1>Hello react world welcome on board</h1>
+      <p>hello Abuja</p>
+    </div>
+  );
+};
+const MyFirstComponent = function () {
+  return <h1>This is my first component created</h1>;
+};
+const NavigationBar = function () {
+  return (
+    <nav>
+      <Navlink />
+    </nav>
+  );
+};
+const Navlink = function () {
+  return <a>Home</a>;
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <MyFirstComponent />
+    <App />
+    <App />
+    <App />
+    <NavigationBar />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
