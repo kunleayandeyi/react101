ed14ecded395613443f366a590c559fb1e1f7513
ksquare <kunleayandeyi@gmail.com>
2024-08-19 22:16:02 +0100

    intro to state

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
 import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 const App = function () {
-  const previousButtonHandler=function(){
-    alert('previous button clicked')
-  }
-  const nextButtonHandler=function(){
-    alert('next button clicked')
-  }
+  const [step, setStep] = useState(1); //step is just the state variable,setStep is a setter function
+  const previousButtonHandler = function () {
+    alert("previous button clicked");
+  };
+  const nextButtonHandler = function () {
+    setStep(step+1)
+  };
   return (
     <div>
       <button className="close">&times;</button>(
       <div className="steps">
         <div className="numbers">
-          <div>1</div>
+          <div className="active">1</div>
           <div>2</div>
           <div>3</div>
         </div>
-        <p className="message">Step 1: learn react</p>
+        <p className="message">
+          Step {step}: {messages[1]}
+        </p>
         <div className="buttons">
-          <button onClick={previousButtonHandler} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          <button
+            onClick={previousButtonHandler}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
             Previous
           </button>
-          <button onClick={nextButtonHandler} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          <button
+            onClick={nextButtonHandler}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
             Next
           </button>
         </div>
